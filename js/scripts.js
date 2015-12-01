63a4a83ea93bd622525675531a873f6ede46ca17
Daniel Huang <danielhuang@g.ucla.edu>
2015-11-30 17:32:31 -0800

    initial framework part 1

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Daniel on 11/30/2015.
+ */
+jQuery(document).ready(function(){
+    $('#home').on('click',function(){scrollTo('#top');})
+    $('.glyphicon-menu-down').on('click',function(){
+       scrollTo('#gallery');
+    });
+});
+
+function scrollTo(id){
+    $('html, body').animate({
+        scrollTop:$(id).offset().top - 90
+    },2000);
+}
+
+function buildScroll(tag){
+    $(tag).on('click', function(e){
+        e.preventDefault();
+        scrollTo(tag + 'h');
+    });
+}
